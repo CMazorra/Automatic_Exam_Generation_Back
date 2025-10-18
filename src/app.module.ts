0c9d553fb1e53ae83bbd3e68fcaef6ba2a1c0e3e
Darian <133162157+Srdariansitop@users.noreply.github.com>
2025-10-18 10:08:25 -0400

    feat(topic): add createTopic endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TopicModule } from './topic/topic.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TopicModule],
+  controllers: [AppController,],
   providers: [AppService],
 })
 export class AppModule {}

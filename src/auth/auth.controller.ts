7da349efcb871eaede71becd8861f156869ba4cc
abg555 <boueadriana2@gmail.com>
2025-11-29 14:14:23 -0500

    auth/me
    auth/me

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, UseInterceptors } from '@nestjs/common';
+import { Controller, Post, Get, Body,Req, UseInterceptors, UseGuards } from '@nestjs/common';
 import { AuthService} from './auth.service';
 import { CookieInterceptor } from './cookie.interceptor';
 import { LoginAuthDto } from './dto/login-auth.dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 
 @Controller('auth')
@@ -21,4 +22,11 @@ export class AuthController {
   async logout() {
     return this.authService.logout();
   }
+  
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  getProfile(@Req() req) {
+    return req.user;  // ← Esto devuelve Darío u otro usuario logueado
+  }
+
 }
